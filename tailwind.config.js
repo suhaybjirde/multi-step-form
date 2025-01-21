9e4ed8023a250af0b4a0f85dd0bb779b6da1adbb
suhaybjirde <dribblekayga@gmail.com>
2025-01-21 20:22:26 +0300

    setup colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      Ubuntu: ["Ubuntu", 'sans-serif'],
+    },
+    colors: {
+      "white": "hsl(0, 0%, 100%)",
+      "primary": {
+        100: "hsl(354, 84%, 57%)",
+        200: "hsl(206, 94%, 87%)",
+        300: "hsl(228, 100%, 84%)",
+        400: "hsl(243, 100%, 62%)",
+        500: "hsl(213, 96%, 18%)"
+      },
+      "neutral": {
+        100: "hsl(231, 100%, 99%)",
+        200: "hsl(217, 100%, 97%)",
+        300: "hsl(229, 24%, 87%)",
+        400: "hsl(231, 11%, 63%)"
+      }
+    }
   },
   plugins: [],
 }
