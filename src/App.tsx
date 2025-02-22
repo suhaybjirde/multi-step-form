197b60d3dfefcc1148a411ef17d34567c9f35e24
suhaybjirde <dribblekayga@gmail.com>
2025-02-22 17:49:47 +0300

    Refactor navigation buttons

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,29 @@ function App() {
   const length = steps.length - 1
   const lastIndex = length == currentIndex
 
-  function goNext() {
+  function validateField(fieldName: keyof FormFields, value: string) {
+    const fieldSchema = formSchema.shape[fieldName]; // Get the specific field's schema
+    const result = fieldSchema.safeParse(value);
+
+    setError((prevErrors) => {
+      // prevents to add prevError[feildName] key in the object if fieldName is not already in the error object for re-render perfomance
+      const prev = {...prevErrors}
+      if (!result.success)
+        prev[fieldName] = result.error.format()
+      else
+        if (prev[fieldName])
+          prev[fieldName] = intailError
+      return prev
+    })
+  }
+
+
+  function goBack() {
+    setCurrentIndex(prevIndex => prevIndex > 0 ? prevIndex - 1 : prevIndex)
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
     const validation = formSchema.safeParse(formData)
     if (!validation.success) 
       setError(validation.error.format())
@@ -97,38 +119,17 @@ function App() {
     }
   }
 
-function validateField(fieldName: keyof FormFields, value: string) {
-  const fieldSchema = formSchema.shape[fieldName]; // Get the specific field's schema
-  const result = fieldSchema.safeParse(value);
-
-  setError((prevErrors) => ({
-    ...prevErrors,
-    [fieldName]: result.success ? intailError : result.error.format(),
-  }));
-}
-
-
-  function goBack() {
-    setCurrentIndex(prevIndex => prevIndex > 0 ? prevIndex - 1 : prevIndex)
-  }
-
-  function handleSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    // submit
-    console.log(formData)
-    setCurrentIndex(length)
-  }
-
   return (
     <main className="md:rounded-lg md:p-3 bg-white w-full md:max-w-[850px] h-full">
       <section className="flex  flex-col md:flex-row md:min-h-[520px]">
         <Aside currentIndex={currentIndex}/>
         <div className="md:h-[350px]  w-full  max-w-[440px] md:max-w-full p-10 md:basis-2/3  absolute md:relative md:top-1 md:left-1  bg-white  top-20  left-1/2 md:-translate-x-0 md:-translate-y-0  -translate-x-1/2  rounded-md md:rounded-none  min-h-[400px] md:min-h-[70vh]  shadow-md md:shadow-none">
           {lastIndex ? <Thanks /> : 
-          <form className=" h-full min-h-full relative">
+          <form className=" h-full min-h-full relative" onSubmit={handleSubmit}>
             {steps[currentIndex]}
             <div className="flex  -bottom-32 py-4 md:-bottom-20 absolute w-full">
               {currentIndex > 0 && <button onClick={goBack} type="button" className="font-bold text-neutral-400">Go Back</button>}
-              {currentIndex == length - 1 ? <button onClick={handleSubmit} type="button" className="bg-primary-400 text-white py-2 px-5 rounded-md font-bold ml-auto">Confirm</button> : <button type="button" onClick={goNext} className="bg-primary-500 text-white py-2 px-3 rounded-md font-bold ml-auto">Next Step</button>}
+              {currentIndex == length - 1 ? <button className="bg-primary-400 text-white py-2 px-5 rounded-md font-bold ml-auto">Confirm</button> : <button className="bg-primary-500 text-white py-2 px-3 rounded-md font-bold ml-auto">Next Step</button>}
             </div>
           </form>}
         </div>
