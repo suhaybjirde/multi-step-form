0c4613a59d263b9e4df39d08a00c804d179df9cf
suhaybjirde <dribblekayga@gmail.com>
2025-02-22 17:34:47 +0300

    update error messages and phone validation

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,14 +1,16 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    name: z.string({ required_error: 'This Feild is Required'}).min(5, 'name must be at least 5 characters'),
+    name: z.string({ required_error: 'This Feild is Required'}).min(5, 'Must be at least 5 characters'),
     email: z.string().min(1, "This Field is Required").email(),
     phone: z.string({
         required_error: 'This Field is Required',
       })
-        .min(10, 'Number must be at least 10 digits')
-        .max(15, 'Number must be at most 15 digits')
-        .regex(/^\d+$/, 'Only digits are allowed'),
+        .min(10, 'Must be at least 10 digits')
+        .max(15, 'Must be at most 15 digits')
+        .regex(/^\+\d{10,15}$/, {
+          message: "Must start with '+' followed by 10 to 15 digits.",
+        }),
     plan: z.enum(['arcade', 'advanced', 'pro']),
     planTime: z.enum(['monthly', 'yearly']),
     onlineService: z.boolean(),
